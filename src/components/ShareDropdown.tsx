a0b71c290fcd36154c85b721b8761e3463fc6bfa
Marcelo <Marceloprego1223@gmail.com>
2026-06-23 14:00:26 -0300

    fix: align share dropdown instagram color with pitaco palette

--- src/components/ShareDropdown.tsx
+++ src/components/ShareDropdown.tsx
@@ -186,7 +186,7 @@ export function ShareDropdown({
             disabled={isLoading} 
             className="cursor-pointer focus:bg-gray-800 focus:text-white py-2.5"
           >
-            <Instagram className="w-4 h-4 mr-3 text-pink-500" />
+            <Instagram className="w-4 h-4 mr-3 text-[#E3C275]" />
             Instagram
           </DropdownMenuItem>
         )}
